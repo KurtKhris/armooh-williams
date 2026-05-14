644da3187846f34515066df9d34b2c2be901ec7b
KurtKhris <edem.kpegah@htu.edu.gh>
2026-05-14 17:25:06 +0000

    ok4

--- src/components/admin/RichTextEditor.tsx
+++ src/components/admin/RichTextEditor.tsx
@@ -82,7 +82,7 @@ export default function RichTextEditor({ value, onChange, placeholder }: Props) 
 
   useEffect(() => {
     if (editor && value !== editor.getHTML()) {
-      editor.commands.setContent(value, false);
+      editor.commands.setContent(value, { emitUpdate: false });
     }
   }, [value, editor]);
 
